2a5de4a972fbce9b742001f63c32f0135152222d
unknown <quentin@bonaventure.xyz>
2020-11-27 15:32:46 +0100

    Sets better 'power button' for servers status

--- lib/mppm_web/views/dashboard_view.ex
+++ lib/mppm_web/views/dashboard_view.ex
@@ -6,13 +6,13 @@ defmodule MppmWeb.DashboardView do
   def get_available_titlepacks(), do: Mppm.ServerConfig.get_available_titlepacks()
 
   def get_status_icon(:started, login), do:
-    tag(:input, type: "button", value: "🟢", phx_click: "stop-server", phx_target: "#server-#{login}")
+    tag(:img, src: "/images/web_ui/power-button-on.svg", phx_click: "stop-server", phx_target: "#server-#{login}")
   def get_status_icon(:starting, _login), do:
-    "🟡"
+    tag(:img, src: "/images/web_ui/power-button-starting.svg")
   def get_status_icon(:stopped, login), do:
-    tag(:input, type: "button", value: "🔴", phx_click: "start-server", phx_target: "#server-#{login}")
+    tag(:img, src: "/images/web_ui/power-button-off.svg", phx_click: "start-server", phx_target: "#server-#{login}")
   def get_status_icon(:failed, _login), do:
-    "X"
+    tag(:img, src: "/images/web_ui/power-button-failed.svg")
   def get_status_icon(_, _login), do:
     "?"
 
