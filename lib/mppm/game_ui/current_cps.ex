9052f7c44d1d37c617e98bfd06243ace87fdde92
QBonaventure <q.bonaventure@gmail.com>
2024-09-16 13:13:14 +0200

    GameUI.CurrentCPS general improvement

--- lib/mppm/game_ui/current_cps.ex
+++ lib/mppm/game_ui/current_cps.ex
@@ -3,26 +3,16 @@ defmodule Mppm.GameUI.CurrentCPs do
 
   @behaviour Mppm.GameUI.Module
 
+  # Sets how much of the previous CPs and total of it to show
+  @before 2
+  @max 10
+
+  # Sets widget columns position for players' names, times and CPs position.
+  @player_pos "22 0"
+  @time_pos "21 0"
+  @cp_n_pos "6 0"
+
   def name, do: "CurrentCPs"
-
-
-  def compare(cps, cp_number, time, user_login)
-  when is_map_key(cps, cp_number) do
-    case time < Kernel.get_in(cps, [cp_number, :time]) do
-      true ->
-        user = Mppm.ConnectedUsers.get_user(user_login)
-        updated_cps = Map.put(cps, cp_number, %{player: user.nickname, time: time})
-        {:updated, updated_cps}
-      false ->
-        {:unchanged, cps}
-    end
-  end
-
-  def compare(cps, cp_number, time, user_login) do
-    user = Mppm.ConnectedUsers.get_user(user_login)
-    updated_cps = Map.put(cps, cp_number, %{player: user.nickname, time: time})
-    {:updated, updated_cps}
-  end
 
   ##############################################################################
   ############################ GenServer Callbacks #############################
@@ -30,28 +20,48 @@ defmodule Mppm.GameUI.CurrentCPs do
 
   def handle_info({:loaded_map, server_login, _map_uid}, state) do
     clean_cps = %{}
-    update_table(state.server_login, clean_cps)
+    update_table(clean_cps)
     |> Mppm.GameUI.Helper.send_to_all(server_login)
 
     {:noreply, %{state | checkpoints: clean_cps}}
   end
 
   def handle_info({:user_connected, server_login, user}, state) do
-    update_table(state.server_login, state.checkpoints)
+    update_table(state.checkpoints)
     |> Mppm.GameUI.Helper.send_to_user(server_login, user.login)
-    {:noreply, state}
+
+    players_position =
+      state.players_position
+      |> Map.put(user.id, 0)
+    {:noreply, %{state | players_position: players_position}}
   end
 
 
   def handle_info({:player_waypoint, server_login, user_login, cp_number, time}, state) do
-   case compare(state.checkpoints, cp_number, time, user_login) do
-     {:updated, updated_cps} ->
-       update_table(state.server_login, updated_cps)
-       |> Mppm.GameUI.Helper.send_to_all(server_login)
-       {:noreply, %{state | checkpoints: updated_cps}}
-    {:unchanged, _cps} ->
-      {:noreply, state}
+    {status, cps} = update_cps(state.checkpoints, cp_number, time, user_login)
+    update_table(cps, cp_number)
+    |> Mppm.GameUI.Helper.send_to_user(server_login, user_login)
+
+    case status do
+      :updated -> {:noreply, %{state | checkpoints: cps}}
+      :unchanged -> {:noreply, state}
     end
+  end
+  def handle_info({:player_end_lap, server_login, user_login, cp_number, time}, state) do
+    {status, cps} = update_cps(state.checkpoints, cp_number, time, user_login)
+    update_table(cps, cp_number)
+    |> Mppm.GameUI.Helper.send_to_user(server_login, user_login)
+
+    case status do
+      :updated -> {:noreply, %{state | checkpoints: cps}}
+      :unchanged -> {:noreply, state}
+    end
+  end
+
+  def handle_info({:player_giveup, server_login, user_login}, state) do
+    update_table(state.checkpoints)
+    |> Mppm.GameUI.Helper.send_to_user(server_login, user_login)
+    {:noreply, state}
   end
 
   def handle_info(_message, state) do
@@ -72,16 +82,18 @@ defmodule Mppm.GameUI.CurrentCPs do
 
   def start_link([server_login], _opts \\ []),
     do: GenServer.start_link(__MODULE__, [server_login], name: {:global, {__MODULE__, server_login}})
+
   def init([server_login]) do
     Process.flag(:trap_exit, true)
     :ok = Phoenix.PubSub.subscribe(Mppm.PubSub, "race-status")
     :ok = Phoenix.PubSub.subscribe(Mppm.PubSub, "maps-status")
     :ok = Phoenix.PubSub.subscribe(Mppm.PubSub, "players-status")
-    {:ok, %{server_login: server_login, checkpoints: %{}}}
+    {:ok, %{server_login: server_login, checkpoints: %{}, players_position: %{}}}
   end
 
   def terminate(_reason, state) do
-    Mppm.GameUI.Helper.send_to_all(root_wrap(), state.server_login)
+    root_wrap()
+    |> Mppm.GameUI.Helper.send_to_all(state.server_login)
     :normal
   end
 
@@ -90,36 +102,82 @@ defmodule Mppm.GameUI.CurrentCPs do
   ############################# Private Functions ##############################
   ##############################################################################
 
-  defp root_wrap(content \\ []) do
+  defp update_cps(cps, cp_number, time, user_login)
+  when is_map_key(cps, cp_number+1) do
+    case time < Kernel.get_in(cps, [cp_number+1, :time]) do
+      true ->
+        user = Mppm.ConnectedUsers.get_user(user_login)
+        updated_cps = Map.put(cps, cp_number+1, %{player: user.nickname, time: time})
+        {:updated, updated_cps}
+      false ->
+        {:unchanged, cps}
+    end
+  end
+  defp update_cps(cps, cp_number, time, user_login) do
+    user = Mppm.ConnectedUsers.get_user(user_login)
+    updated_cps = Map.put(cps, cp_number+1, %{player: user.nickname, time: time})
+    {:updated, updated_cps}
+  end
+
+
+  defp update_table(cps), do: update_table(cps, 0)
+  defp update_table(cps, cp_number) do
+    position = cp_number+1
+    cps_to_show =
+      case (cp_number - @before) < 0 do
+        true -> Enum.take(cps, @max)
+        false ->
+          cps
+          |> Enum.drop(cp_number-@before)
+          |> Enum.take(@max)
+      end
+
+    Enum.map(cps_to_show, fn {cp_pos, cp} ->
+      case cp_pos == position do
+        true -> {cp_pos, Map.put(cp, :status, :current)}
+        false -> {cp_pos, Map.put(cp, :status, :none)}
+      end
+    end)
+    |> root_wrap()
+  end
+
+  defp root_wrap(content \\ [], position \\ 0) do
+    {content, index} =
+      Enum.map_reduce(content, 1, fn
+        {position, cp}, acc ->
+          {checkpoint_row(acc, position, cp), acc+1}
+      end)
     {:manialink, [id: "current-cps", version: 3], [
       Mppm.GameUI.Stylesheet.get_stylesheet(),
       {:frame, [pos: "122 80"], [
-        {:label, [text: "CP", class: "header-text", pos: "4 0"], []},
-        {:label, [text: "Time", class: "header-text", pos: "15 0"], []},
-        {:label, [text: "Player", class: "header-text", pos: "22 0", halign: "left"], []},
+        {:label, [text: "CP", class: "header-text", pos: @cp_n_pos, halign: "right"], []},
+        {:label, [text: "Time", class: "header-text", pos: @time_pos, halign: "right"], []},
+        {:label, [text: "Player", class: "header-text", pos: @player_pos, halign: "left"], []},
+        {:quad, [size: "36 4.5", pos: "1 1", class: "background-quad"], []},
       ]},
-      {:frame, [pos: "122 76.5"], content},
-      {:quad, [size: "36 4.5", pos: "123 81", class: "background-quad"], []},
+      {:frame, [pos: "122 75.5"], content}
     ]
   }
   end
 
+  defp checkpoint_row(index, position, cp) do
+    frame_attrs = %{
+      id: "cp-data-"<>Integer.to_string(index),
+      "z-index": 10,
+      pos: "0 -"<> Float.to_string((index-1)*4.5)
+    }
+    |> Map.to_list()
 
-  def update_table(_server_login, cps) do
-    cps_manialink =
-      cps
-      |> Enum.map(&checkpoint_container/1)
-
-    root_wrap(cps_manialink)
+    content = [
+      {:quad, Map.to_list(%{size: "36 4.5", pos: "1 1", bgcolor: cp_bgcolor(cp.status), "z-index": 2}), []},
+      {:label, Map.to_list(%{text: Integer.to_string(position), class: "text", pos: @cp_n_pos, halign: "right", "z-index": 10}), []},
+      {:label, Map.to_list(%{text: Mppm.TimeRecord.to_string(cp.time), class: "text", pos: @time_pos, halign: "right", "z-index": 10}), []},
+      {:label, Map.to_list(%{text: cp.player, class: "text", pos: @player_pos, halign: "left", "z-index": 10}), []}
+    ]
+    {:frame, frame_attrs, content}
   end
 
-  def checkpoint_container({cp_number, %{time: time, player: player_nickname}}) do
-    {:frame, [id: Integer.to_string(cp_number), size: "37 4.5", pos: "0 -"<> Float.to_string(cp_number*4.5)], [
-      {:label, [text: Integer.to_string(cp_number+1)<>".", class: "text", pos: "5 -0.7", halign: "right"], []},
-      {:label, [text: Mppm.TimeRecord.to_string(time), class: "text", pos: "18 -0.7", halign: "right"], []},
-      {:label, [text: player_nickname, class: "text", pos: "22 -0.7", halign: "left"], []},
-      {:quad, [size: "36 4.5", pos: "1 0", class: "background-quad-black"], []}
-    ]}
-  end
+  defp cp_bgcolor(:none), do: "222"
+  defp cp_bgcolor(:current), do: "888"
 
 end
